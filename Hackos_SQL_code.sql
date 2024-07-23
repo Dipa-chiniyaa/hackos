87d58548526a95f0200e9b78564499a38db7e0f2
Dipa Chiniya Parasaram <157943824+Dipa-chiniyaa@users.noreply.github.com>
2024-07-23 20:00:37 +0530

    Update Hackos_SQL_code.sql

--- Hackos_SQL_code.sql
+++ Hackos_SQL_code.sql
@@ -2,3 +2,31 @@
 -- and here country code for america is 'USA'
 SELECT * FROM CITY
 WHERE POPULATION > 100000 and COUNTRYCODE = 'USA';
+
+--  all american cities with population larger than 120000 and with american country code 'USA'
+
+SELECT NAME FROM CITY
+WHERE POPULATION > 120000 and COUNTRYCODE = 'USA';
+
+-- all column for every row in CITY table
+SELECT * FROM CITY;
+
+-- all column for every row in CITY table where ID is 1661
+SELECT * FROM CITY
+WHERE ID = 1661;
+
+-- all column for every japanese CITY table where country code is JPN
+SELECT * FROM CITY
+WHERE COUNTRYCODE = 'JPN';
+
+-- names of all the japanese CITY table where country code is JPN
+SELECT NAME FROM CITY
+WHERE COUNTRYCODE = 'JPN';
+
+-- List CIty and state from station table
+SELECT CITY,STATE FROM STATION;
+
+-- list of Distinct city names from STATION table where ID is Even
+SELECT DISTINCT CITY FROM STATION
+WHERE MOD(ID,2) = 0;
+
