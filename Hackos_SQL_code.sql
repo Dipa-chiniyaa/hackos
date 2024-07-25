d07e206bf2e4d3f45d26e6f67eee12eaf0eef795
Dipa Chiniya Parasaram <157943824+Dipa-chiniyaa@users.noreply.github.com>
2024-07-25 20:07:16 +0530

    Update Hackos_SQL_code.sql

--- Hackos_SQL_code.sql
+++ Hackos_SQL_code.sql
@@ -30,3 +30,31 @@ SELECT CITY,STATE FROM STATION;
 SELECT DISTINCT CITY FROM STATION
 WHERE MOD(ID,2) = 0;
 
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+
+-- find difference between total no. of cities and no. distinct City
+
+SELECT COUNT(CITY) - COUNT(DISTINCT(CITY))
+FROM STATION ;
+
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+
+-- shortest and longest CITY name
+
+-- SELECT CITY ,Max( LENGTH(CITY)) FROM STATION
+-- ORDER BY CITY ASC;
+-- SELECT CITY, MIN(LENGTH(CITY)) FROM STATION
+-- ORDER BY CITY ASC;
+
